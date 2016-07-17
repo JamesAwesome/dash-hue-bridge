a922605b000e61be094e8cc46f459b16931a000c
James Awesome <james@wesome.nyc>
2016-07-17 14:53:34 -0400

    Add license

---
+++ README.md
@@ -0,0 +1,15 @@
+# dash-hue-bridge
+
+An Amazon Dash Button to Phillips Hue Bridge
+
+## Usage:
+
+```bash
+git clone git@git://github.com:/JamesAwesome/dash-hue-bridge
+cd ./dash-hue-bridge
+npm install
+./app.js -h
+```
+### Known Issues
+
+On OSX you have to use sudo. Listen, I didn't write BSD.
--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 
 const chalk = require('chalk');
 const meow = require('meow');
-const dash_button = require("node-dash-button");
+const dash_button = require('node-dash-button');
+const hue_api = require('node-hue-api').HueApi;
 
 const cli = meow(`
     Usage
-      $ app.js <Mac Address>
+      $ app.js <Dash Mac Address> <Hue Bridge Hostname> <Hue UserID>
     Options
       --help, -h  Print usage and exit
 
@@ -19,12 +20,39 @@ const cli = meow(`
     }
 });
 
-var mac_address = cli.input[0];
+var dash_mac_address = cli.input[0];
+var hue_bridge = cli.input[1];
+var hue_user = cli.input[2];
+var dash = dash_button(dash_mac_address);
 
-var dash = dash_button(mac_address);
+var display_result = function (result) {
+    console.log(chalk.green(JSON.stringify(result, null, 2)));
+};
 
-console.log(chalk.blue('Attempting to use dash button at: ' + mac_address));
+var display_error = function(err) {
+    console.log(chalk.red(err));
+};
+
+var hue = new hue_api(hue_bridge, hue_user);
+var state = require('node-hue-api').lightState.create()
+var on = false
 
 dash.on('detected', function (dash_id) {
   console.log(chalk.blue('Dash button ' + dash_id + ' was clicked!'));
+
+  if (!on) {
+      // Turn lights off
+      hue.setGroupLightState(1, state.off())
+          .then(display_result)
+          .fail(display_error)
+          .done();
+  } else {
+      // Turn Lights On
+      hue.setGroupLightState(1, state.hue(16356).bri(254).on())
+          .then(display_result)
+          .fail(display_error)
+          .done();
+  }
+
+  on = !on;
 });
