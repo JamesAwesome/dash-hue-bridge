efabe326792b692687e3db8fcd2ab067eb730c36
James Awesome <james@wesome.nyc>
2016-07-17 11:30:04 -0400

    POC Complete

---
+++ app.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use-strict';
+
+const chalk = require('chalk');
+const meow = require('meow');
+const dash_button = require("node-dash-button");
+
+const cli = meow(`
+    Usage
+      $ app.js <Mac Address>
+
+    Examples
+      $ app.js 00:12:34:56:78:00
+`);
+
+var mac_address = cli.input[0];
+
+var dash = dash_button(mac_address);
+
+console.log(chalk.blue('Attempting to use dash button at: ' + mac_address));
+
+dash.on('detected', function (dash_id) {
+  console.log(chalk.blue('Dash button ' + dash_id + ' was clicked!'));
+});
